9a0d319f68c3ce1b1f30595178764b3c6c7bd205
Phil Scott <me@philscotted.com>
2014-12-01 15:00:51 +0000

    First set of axioms and problem for topology from TPTP.

---
+++ src/main/resources/TopProblem1.scala
@@ -0,0 +1,10 @@
+object TopAxioms { 
+  val problem =
+    List(
+      // Name: lemma_1a_1
+      // Role: negated_conjecture
+      List("basis(cx, f)")),
+      // Name: lemma_1a_2
+      // Role: negated_conjecture
+      List("~subset_sets(union_of_members(top_of_basis(f)), cx)")))
+}
