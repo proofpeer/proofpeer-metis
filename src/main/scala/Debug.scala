b9a79e2c919d559e7c7401769a29656a31c64349
Phil Scott <me@philscotted.com>
2015-02-05 18:36:16 +0000

    Simple profiler.

--- src/main/scala/Debug.scala
+++ src/main/scala/Debug.scala
@@ -31,4 +31,12 @@ object Debug {
     }
     System.out.println("")
   }
+
+  def profile[A](name: String, x: => A) = {
+    val start = System.nanoTime()
+    val theX = x
+    val end = System.nanoTime()
+//    System.out.println(name + ": " + (end - start)/1000000000.0)
+    theX
+  }
 }
