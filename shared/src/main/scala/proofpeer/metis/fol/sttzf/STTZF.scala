c5e8feecfb3ecae02cbb20f45a61898951e25d7e
Phil Scott <me@philscotted.com>
2016-09-22 21:16:34 +0100

    Reduce bracketing.

--- shared/src/main/scala/proofpeer/metis/fol/sttzf/STTZF.scala
+++ shared/src/main/scala/proofpeer/metis/fol/sttzf/STTZF.scala
@@ -13,11 +13,11 @@ object Axioms {
     SExpr.folOfSExpr(SexpParser.parse(str),true)
 
   val extensionality =
-    folOfString("(<-> ((=) a b) (! x (<-> ((vin) x A) ((vin) x B))))")
+    folOfString("(<-> (= a b) (! x (<-> (vin x A) (vin x B))))")
   val upair =
-    folOfString("(ex P (! x (<-> ((vin) x P) (or ((=) x a) ((=) y a)))))")
+    folOfString("(ex P (! x (<-> (vin x P) (or (= x a) (= y a)))))")
   val funion =
-    folOfString("(ex U (! x (<-> ((vin) x P) (or ((vin) x A) ((vin) x B)))))")
+    folOfString("(ex U (! x (<-> (vin x P) (or (vin x A) (vin x B)))))")
   val powerset =
-    folOfString("(ex P (! X (<-> ((vin) X P) (! x (-> ((vin) x X) ((vin) x A))))))")
+    folOfString("(ex P (! X (<-> (vin X P) (! x (-> (vin x X) (vin x A))))))")
 }
