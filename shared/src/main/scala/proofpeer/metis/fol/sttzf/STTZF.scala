e3356992a70389450000a9a6878349cb8a334c58
Phil Scott <me@philscotted.com>
2016-09-23 11:55:10 +0100

    Make separation schematic.

--- shared/src/main/scala/proofpeer/metis/fol/sttzf/STTZF.scala
+++ shared/src/main/scala/proofpeer/metis/fol/sttzf/STTZF.scala
@@ -1,10 +1,11 @@
 package proofpeer.metis.fol.sttzf
 
 import org.ensime.sexp.SexpParser
-import org.ensime.sexp.Sexp
 import scalaz._
 import Scalaz._
+import org.ensime.sexp.{ Sexp, SexpCons, SexpNumber }
 
+import proofpeer.metis.Term
 import proofpeer.metis.fol._
 
 object Axioms {
@@ -19,5 +20,10 @@ object Axioms {
   val funion =
     folOfString("(ex U (! x (<-> (vin x P) (or (vin x A) (vin x B)))))")
   val powerset =
-    folOfString("(ex P (! X (<-> (vin X P) (! x (-> (vin x X) (vin x A))))))")
+    folOfString("(ex P (! X (<-> ((vin) X P) (! x (-> ((vin) x X) ((vin) x A))))))")
+  def separation(
+    inst: (Sexp, List[Term[Sexp,Sexp]]) => FOL[Sexp,Sexp,Sexp,FOL.Neg,FOL.Binder]) =
+    folOfString("(ex S (! x (<-> (vin x S) (and (vin x A) (P x)))))").map {
+      fol => FOL.instPred(fol)(inst)
+    }
 }
