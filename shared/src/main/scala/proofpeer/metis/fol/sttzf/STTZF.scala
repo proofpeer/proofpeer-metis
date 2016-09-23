733170f42f4c5f6cf703f066f95b14285cc6fb10
Phil Scott <me@philscotted.com>
2016-09-23 11:55:41 +0100

    S-expression CNF coversion.

--- shared/src/main/scala/proofpeer/metis/fol/sttzf/STTZF.scala
+++ shared/src/main/scala/proofpeer/metis/fol/sttzf/STTZF.scala
@@ -1,12 +1,15 @@
 package proofpeer.metis.fol.sttzf
 
 import org.ensime.sexp.SexpParser
-import scalaz._
-import Scalaz._
 import org.ensime.sexp.{ Sexp, SexpCons, SexpNumber }
 
 import proofpeer.metis.Term
 import proofpeer.metis.fol._
+
+import FOL.Instances._
+
+import scalaz._
+import Scalaz._
 
 object Axioms {
   def folOfString(str: String):
@@ -26,4 +29,14 @@ object Axioms {
     folOfString("(ex S (! x (<-> (vin x S) (and (vin x A) (P x)))))").map {
       fol => FOL.instPred(fol)(inst)
     }
+
+  def unfresh(x: FOL.Fresh[Sexp]) = SexpCons(x.origin, SexpNumber(x.get))
+
+  def cnfSexp(fol: FOL[Sexp, Sexp, Sexp, FOL.Neg, FOL.Binder]) = {
+    val (bnds,mat,_) = Matrix.quantPull(FOL.toNNF(fol))
+    val bnds2 = bnds.map { _.rightMap(unfresh(_)) }
+    val mat2 = FOL.trimap(mat)(v => v.fold(v => v, unfresh(_)),f => f,p => p)
+    val mat3 = Matrix.skolemize(bnds2, mat2)
+    CNF.cnf(FOL.trimap(mat3)(v => v, _.fold(v => v, f => f), p => p))
+  }
 }
